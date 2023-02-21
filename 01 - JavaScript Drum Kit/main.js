47bc4bac124847e7da33f4b49a40fcd5d305d096
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-02-21 17:19:26 +0100

    final

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,23 +1,16 @@
-window.addEventListener('keydown', playSound)
-
-
-function playSound(e){
-    let audio = document.querySelector(`audio[data-key ="${e.keyCode}"]`)
-    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-
-    
-    if(!audio){
-        return
-    }
-
-    key.classList.add('playing')
-    audio.currentTime = 0
+window.addEventListener('keydown', (event) => {
+    let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    audio.currentTime = 0;
     audio.play()
-}
+
+    let key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
+    key.classList.add('playing')
+})
 
 let keys = document.querySelectorAll('.key')
 keys.forEach(key => {
-    key.addEventListener('transitionend', () => {
+    key.addEventListener('transitionend', ()=>{
         key.classList.remove('playing')
     })
 })
+
